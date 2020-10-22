7efba4a4afba91ac1106dd63cbe9933451a2e525
TIIEHenry <1806968131@qq.com>
2020-10-22 20:33:28 +0800

    adapte new version

--- app/src/main/java/taokdao/plugins/engine/rhino/controller/ProjectTemplateController.kt
+++ app/src/main/java/taokdao/plugins/engine/rhino/controller/ProjectTemplateController.kt
@@ -1,19 +1,8 @@
 package taokdao.plugins.engine.rhino.controller
 
-import taokdao.api.data.bean.Properties
 import taokdao.api.main.IMainContext
-import taokdao.api.main.action.MainAction
 import taokdao.api.plugin.bean.PluginManifest
 import taokdao.api.plugin.entrance.BaseDynamicPluginEntrance
-import taokdao.api.template.project.ProjectTemplatePool
-import taokdao.api.template.project.wrapped.ProjectTemplate
-import taokdao.plugins.engine.rhino.PluginConstant
-import taokdao.plugins.engine.rhino.R
-import taokdao.plugins.setup.io.Filej
-import tiiehenry.android.ui.dialogs.api.callback.InputCallback
-import tiiehenry.android.ui.dialogs.api.strategy.Dialogs
-import java.io.File
-import java.io.IOException
 
 class ProjectTemplateController: BaseDynamicPluginEntrance() {
 
